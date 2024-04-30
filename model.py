d6e0ba116894a8006ccae8ad546d95bffddcd74a
0xnrous <0xnrous@gmail.com>
2024-04-30 14:52:25 +0300

    #2model-tuning-0.71𖧵

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from data_preparation import prepare_data   
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 def build_model():
     # we need to train and save the model 
     
@@ -9,10 +10,11 @@ def build_model():
     data = prepare_data()
     # 2- identify the X and y variables
     X , y = get_X_y(data)
-    df_split = split_data(X , y)
-    model_score = train_model(df_split[0] , df_split[1] , df_split[2] , df_split[3])
-    
-    return model_score
+    X_train , X_test ,y_train , y_test  = split_data(X , y)
+    Grid_rf = train_model(X_train, y_train)
+    evalute_score = evalute_model(Grid_rf , X_test , y_test)
+    #print(r'Model Evalute Score : ' , evalute_score)
+    return r'Model Evalute Score : ' , evalute_score
     
 def get_X_y(data):
     X = data[['area' ,
@@ -34,16 +36,37 @@ def split_data(X , y):
     X_train , X_test , y_train , y_test = train_test_split(X , y , test_size= 0.2 , random_state = 42)
     return X_train , X_test , y_train , y_test
 
-
-
-
-def train_model(X_train, X_test , y_train , y_test):
-    rf = RandomForestRegressor()
-    rf.fit(X_train , y_train)
-    model_score = rf.score(X_test , y_test)
-    return model_score
-    
-    
+# def train_model(X_train, X_test , y_train , y_test):
+#     rf = RandomForestRegressor()
+#     rf.fit(X_train , y_train)
+#     model_score = rf.score(X_test , y_test)
+#     return model_score
+def train_model(X_train , y_train ):
+    grid_param = {
+    "n_estimators" : range(50 ,100 , 20),
+    "criterion" : ['mae'], 
+    "max_depth" : range(6 , 12 , 2)
+    }
+    grid = GridSearchCV(
+        RandomForestRegressor() , 
+        param_grid= grid_param , 
+        cv = 5 , 
+        n_jobs = -1
+    )
+    model_grid = grid.fit(X_train , y_train)
+    #best_model_param = model_grid.best_params_
+    #best_model_score = model_grid.best_score_
+    print("Train score: " , model_grid.best_score_)
+    return model_grid.best_estimator_
+def evalute_model(model , X_test , y_test):
+    return model.score(X_test , y_test)    
 # test 
-model_score = build_model()
-print(model_score)
+df_get = build_model()
+print(df_get)
+
+
+'''
+Train score:  0.7143105454150367
+('Model Evalute Score : ', 0.7127687339259972)
+
+'''
