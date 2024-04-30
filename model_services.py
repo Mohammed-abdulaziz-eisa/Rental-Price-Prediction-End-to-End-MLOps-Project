131df31857faaf8335bbda71757db8fd75aa7ed7
0xnrous <0xnrous@gmail.com>
2024-04-30 15:59:15 +0300

    MLops-services(load-predict)♻︎

--- model_services.py
+++ model_services.py
@@ -1,24 +1,20 @@
 from pathlib import Path
+import pickle as pk
+
 from model import build_model
-import pickle as pkl
-class ModelServices:
+
+class ModelService:
     def __init__(self):
         self.model = None
-        
-        
-    def load_model(self , model_name = "hypered_rf.pkl"):
-        model_path = Path("/model/hypered_rf.pkl")
-        
+
+    def load_model(self, model_name='hypered_tuned_rf.pkl'):
+        model_path = Path(f'model/{model_name}')
+
         if not model_path.exists():
             build_model()
-            
-        self.model = pkl.load('./model/hypered_rf.pkl' , 'rb')
-        
-        
-    def predict(self , input_parameters):
+
+        self.model = pk.load(open(f'model/{model_name}', 'rb'))
+
+    def predict(self, input_parameters):
         return self.model.predict([input_parameters])
-
-
-#test
-ml_svr = ModelServices()
-ml_svr.load_model('/model/hypered_rf.pkl')
+    
