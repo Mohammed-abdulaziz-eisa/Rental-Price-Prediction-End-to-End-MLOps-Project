543f20b37051396a5d8525de85f1806932518903
0xnrous <0xnrous@gmail.com>
2024-04-30 16:04:29 +0300

    MLops-services(load-predict)♻︎

--- model_services.py
+++ model_services.py
@@ -7,7 +7,7 @@ class ModelService:
     def __init__(self):
         self.model = None
 
-    def load_model(self, model_name='hypered_tuned_rf.pkl'):
+    def load_model(self, model_name='hypered_rf.pkl'):
         model_path = Path(f'model/{model_name}')
 
         if not model_path.exists():
@@ -18,3 +18,9 @@ class ModelService:
     def predict(self, input_parameters):
         return self.model.predict([input_parameters])
     
+# Test the script
+
+# ml_svc = ModelService()
+# ml_svc.load_model('hypered_rf.pkl')
+# pred = ml_svc.predict([85 , 2015 , 2 ,20 , 1 ,1 ,0 ,0 ,1])
+# print(pred)
