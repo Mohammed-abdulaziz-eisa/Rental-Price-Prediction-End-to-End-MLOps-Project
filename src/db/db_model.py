b049bcf7c11c631ad5213884b5240f168ea38451
0xnrous <0xnrous@gmail.com>
2024-08-23 14:11:07 +0300

    docs: add detailed docstrings for ORM model and configuration

--- src/db/db_model.py
+++ src/db/db_model.py
@@ -1,12 +1,45 @@
+"""
+    This module is used for creating the database model for the rent apartment data.
+    
+    Classes: 
+        - Base : Base class for the database model.
+        - RentApartments : Database model for rent apartment data.
+       
+"""
+
 from sqlalchemy import REAL , INTEGER , VARCHAR
 from sqlalchemy.orm import DeclarativeBase , Mapped , mapped_column
 
 from config.config import settings
 
 class Base(DeclarativeBase):
+    """ Base class for ORM model. """
     pass
 
 class RentApartments(Base):
+    
+    """
+    ORM model representating the RentApartments table in the database.
+    
+    Attributes:
+        address (str): The address of the apartment (Primary Key).
+        area (float): The area the apartment in square meters. 
+        constraction_year (int): The year of the construction of the apartment. 
+        rooms (int): The number of rooms in the apartment.
+        bedrooms (int): The number of the bedrooms in the apartment. 
+        bathrooms (int): The number of the bathrooms in the apartment.
+        balcony (str): The balcony status of the apartment.
+        storage (str): The storage status of the apartment.
+        parking (str): The parking status of the apartment.
+        furnished (str): The furnished status of the apartment. 
+        garage (str): The garage status of the apartment.
+        garden (str): The garden status of the apartment. 
+        energy (str): The energy label of the apartment.
+        facilities (str): Additional facilities in or near the apartment.
+        zip (str): The postal code of the apartment.
+        neighborhood (str): The neighborhood where the apartment is located.
+        rent (int): The rent price of the apartment. 
+    """
     __tablename__ = settings.rent_apart_table_name
 
     address: Mapped[str] = mapped_column(VARCHAR(), primary_key=True)  
